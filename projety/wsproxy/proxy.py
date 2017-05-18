c6a2e09d5f7e5630f1454630cc7b1d731f5f78a8
Alkivi <admin@alkivi.fr>
2017-05-18 03:03:29 +0200

    fix(wsproxy): update on engine.io

--- projety/wsproxy/proxy.py
+++ projety/wsproxy/proxy.py
@@ -60,7 +60,7 @@ class WsProxy(object):
         for mode in modes:
             try:
                 self.async = importlib.import_module(
-                    'engineio.async_' + mode).async
+                    'engineio.async_' + mode)._async
                 self.async_mode = mode
                 break
             except ImportError:
